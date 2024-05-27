663a2d32cbc4e2e9085acbbcc8b984b49fec4f06
Hungvd08 <hungvdhe161733@fpt.edu.vn>
2024-05-27 23:22:10 +0700

    forget password
    forget password

--- GrammarGuardianProject/src/java/Controller/AuthenticationController.java
+++ GrammarGuardianProject/src/java/Controller/AuthenticationController.java
@@ -43,23 +43,7 @@ public class AuthenticationController extends HttpServlet {
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String url = "";
-        String action = request.getParameter("action") == null ? "" : request.getParameter("action");
-        switch (action) {
-            case "login":
-                Login(request, response);
-                break;
-            case "register":
-                Register(request, response);
-                break;
-            case "verify-otp":
-                Register(request, response);
-                break;
-        }
-    }
+    
 
     /**
      * Returns a short description of the servlet.
@@ -71,87 +55,7 @@ public class AuthenticationController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-    private void Login(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            String url = "views/common/sign-in.jsp";
-            String userName = request.getParameter("userName");
-            String password = request.getParameter("password");
-            HttpSession session = request.getSession(false);
-            AuthenticationDAO authDAO = new AuthenticationDAO();
-            User userLogedIn = authDAO.Login(userName, password);
-            if (userLogedIn != null) {
-                if (userLogedIn.isIsActive() && userLogedIn.isIsCofirm()) {
-                    // Login thanh cong
-                    session.setAttribute("USER", userLogedIn);
-                    session.setAttribute("EMAIL", userLogedIn.getEmail());
-                    // User 
-                    if (userLogedIn.getRoleId() == 1) {
-//                        home(request, response);
-                        url = "views/common/index.jsp";
-                        // Admin
-                    } else if (userLogedIn.getRoleId() == 2) {
-                        url = "admin/dashboard.jsp";
-                    }
-                } else if (!userLogedIn.isIsActive()) {
-                    request.setAttribute("ERRORMESSAGE", "Tài khoản của bạn bị vô hiệu hóa. Vui lòng liên hệ quản trị viên!");
-                } else if (!userLogedIn.isIsCofirm()) {
-                    request.setAttribute("ERRORMESSAGE", "Tài khoản của bạn chưa được xác thực vui lòng xác thông qua email của bạn!");
-                }
-            } else {
-                request.setAttribute("ERRORMESSAGE", "Sai tên đăng nhập hoặc tài khoản, vui lòng thử lại!");
-            }
-            request.getRequestDispatcher(url).forward(request, response);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void Register(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            HttpSession session = request.getSession();
-            String url = "views/common/sign-in.jsp";
-            String userName = request.getParameter("username");
-            String password = request.getParameter("password");
-            String email = request.getParameter("email");
-            String firstName = request.getParameter("firstName");
-            String lastName = request.getParameter("lastName");
-            String phone = request.getParameter("phone");
-
-            // create create modal to signup
-            UserSignUp userSignUp = new UserSignUp();
-            userSignUp.setUserName(userName);
-            userSignUp.setPassword(password);
-            userSignUp.setEmail(email);
-            userSignUp.setFirstName(firstName);
-            userSignUp.setLastName(lastName);
-            userSignUp.setPhone(phone);
-            session.setAttribute("email", email);
-            String link = "http://localhost:9999/GrammarGuardian/auth?action=confirm-email";
-            AuthenticationDAO authDAO = new AuthenticationDAO();
-            int result = authDAO.Register(userSignUp);
-
-            if (result == 1) {
-                url = "views/common/sign-in.jsp";
-                request.setAttribute("SUCCESSMESSAGE", "Đăng kí thành công vui lòng xác thực email của bạn");
-                request.setAttribute("EMAIL_URL", "https://mail.google.com/");
-                session.setAttribute("USERNAME", userName);
-                // mail service
-                MailService mailService = new MailService();
-                mailService.sendMailWithConfirmLink(email, link);
-            } else if (result == 2) {
-                request.setAttribute("ERRORMESSAGE", "UserName đã có người sử dụng hãy thử một tên khác ");
-            } else if (result == 3) {
-                request.setAttribute("ERRORMESSAGE", "Email này đã được sử dụng trên hệ thống vui lòng lựa chọn một email khác");
-            } else {
-                request.setAttribute("ERRORMESSAGE", "Cannot regitser");
-            }
-            request.getRequestDispatcher(url).forward(request, response);
-
-        } catch (Exception e) {
-            System.out.println("Register" + e.getMessage());
-        }
-    }
+    
 
     private void ConfirmEmail(HttpServletRequest request, HttpServletResponse response) {
         try {
