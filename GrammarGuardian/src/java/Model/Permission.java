aafdd78bd15bbf7dde9af594812775b07bb93fb1
Hungvd08 <hungvdhe161733@fpt.edu.vn>
2024-05-28 23:06:55 +0700

    update from main
    update from main

---
+++ GrammarGuardian/src/java/Model/Permission.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Datnt
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission {
+    private int Id;
+    private int UserId;
+    private int LimitText;
+    private int CheckTime;
+}
