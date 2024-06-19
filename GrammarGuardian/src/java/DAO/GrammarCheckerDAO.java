8077febaa5260fe91514462c272af5d17da936f5
Hungvd08 <hungvdhe161733@fpt.edu.vn>
2024-06-19 11:33:08 +0700

    get all favorite post

--- GrammarGuardian/src/java/DAO/GrammarCheckerDAO.java
+++ GrammarGuardian/src/java/DAO/GrammarCheckerDAO.java
@@ -168,4 +168,48 @@ public class GrammarCheckerDAO extends DBContext {
         return false;
     }
     
+     public int getAllFavourtePostTotal(int userId) {
+        try {
+            // status = 3 was post manager approval
+            String sql = "SELECT COUNT(*) FROM [Post] p JOIN [Post_Favourite] pf ON p.PostId = pf.PostId AND p.UserId = ?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, userId);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    // list user manager approve
+    public List<Post> getAllFavourtePost(int userId, int index) {
+        List<Post> listPosts = new ArrayList();
+        try {
+            // status = 3 was post manager approval
+            String sql = "SELECT p.UserId, p.PostId, p.Title, p.Description, p.CreateAt, p.Status FROM [Post] p JOIN [Post_Favourite] pf ON p.PostId = pf.PostId AND pf.UserId = ?"
+                    + " ORDER BY PostId DESC OFFSET ? ROWS FETCH NEXT 12 ROWS ONLY";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, userId);
+            ps.setInt(2, (index - 1) * 12);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Post post = new Post();
+                post.setPostId(rs.getInt("PostId"));
+                post.setTitle(rs.getString("Title"));
+                post.setDescription(rs.getString("Description"));
+                post.setStatus(rs.getInt("Status"));
+                post.setCreateAt(rs.getDate("CreateAt").toString());
+                post.setUserId(rs.getInt("UserId"));
+                listPosts.add(post);
+            }
+            return listPosts;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listPosts;
+    }
+    
 }
