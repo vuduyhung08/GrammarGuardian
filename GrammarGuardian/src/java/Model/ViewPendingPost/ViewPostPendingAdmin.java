cc9c51e52b823fe98e074ef4d09c0754cf1eef73
nvthai0611 <nvthai061105@gmail.com>
2024-06-26 14:27:33 +0700

    hainvt update code view post admin

---
+++ GrammarGuardian/src/java/Model/ViewPendingPost/ViewPostPendingAdmin.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.ViewPendingPost;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ViewPostPendingAdmin {
+    private int postId;
+    private String title;
+    private String description;
+    private String createAt;
+    private String useName;
+}
