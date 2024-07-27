6f67732809dbc94190e5f0d039c25d5604937de5
Hungvd08 <hungvdhe161733@fpt.edu.vn>
2024-07-28 00:13:51 +0700

    comit

---
+++ GrammarGuardian/src/java/Model/ViewModel/FeedbackVM.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.ViewModel;
+
+import Model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedbackVM {
+    private int Id;
+    private String Content;
+    private String CreateAt;
+    private int UserId;
+    private int Status;
+    private User User;
+}
---
+++ GrammarGuardian/src/java/Model/ViewModel/ViewUserActive.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.ViewModel;
+
+/**
+ *
+ * @author admin
+ */
+public class ViewUserActive {
+    private int id;
+    private int userId;
+    private String userName;
+    private String avatar;
+    private String fullName;
+    private String email;
+    private String status;
+}
